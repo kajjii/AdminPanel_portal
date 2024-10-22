01aa66ff935add496cf2f5f4f2164efe76e21c15
kajjii <murdiv303@gmail.com>
2024-10-22 18:30:47 +0300

    redux add RTK Query

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,71 +1,62 @@
-import {useHttp} from '../../hooks/http.hook';
-import { useEffect, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+import { useMemo, useCallback } from "react";
+import { useGetHeroesQuery, useDeleteHeroMutation } from "../../api/apiSlice";
 
-import { heroDeleted, fetchHeroes, filteredHeroesSelector } from './heroesSlice';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
-import Spinner from '../spinner/Spinner';
-import './heroesList.scss'
+import Spinner from "../spinner/Spinner";
+import "./heroesList.scss";
 
 const HeroesList = () => {
 
-    // const filteredHeroesSelector = createSelector(
-    //     (state) => state.filters.activeFilter,
-    //     selectAll,
-    //     (filter, heroes) => {
-    //         if(filter === 'all') {
-    //             return heroes
-    //         } else {
-    //             return heroes.filter(item => item.element === filter)
-    //         }
-    //     }
-    // )
+	const {
+		data: heroes = [],
+		isLoading,
+		isError,
+	} = useGetHeroesQuery()
 
-    const filteredHeroes = useSelector(filteredHeroesSelector)
-    const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
-    const dispatch = useDispatch();
-    const {request} = useHttp();
+	const [deleteHero] = useDeleteHeroMutation()
 
-    useEffect(() => {
-        dispatch(fetchHeroes())
-        // eslint-disable-next-line
-    }, []);
+	const activeFilter = useSelector(state => state.filters.activeFilter)
 
-    const onDelete = useCallback((id) => {
-        request(`http://localhost:3001/heroes/${id}`, "DELETE")
-            .then(data => console.log(data, 'Deleted'))
-            .then(dispatch(heroDeleted(id)))
-            .catch(err => console.log(err))
-            // eslint-disable-next-line 
-    }, [request])
+	const filteredHeroes = useMemo(() => {
+		const filteredHeroes = heroes.slice() // создаем копию, чтобы избежать мутаций
 
-    if (heroesLoadingStatus === "loading") {
-        return <Spinner/>;
-    } else if (heroesLoadingStatus === "error") {
-        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
-    }
+		if (activeFilter === "all") {
+		  return filteredHeroes;
+		} else {
+		  return filteredHeroes.filter((item) => item.element === activeFilter);
+		}
+	  }, [heroes, activeFilter])
 
-    const renderHeroesList = (arr) => {
-        if (arr.length === 0) {
-            return <h5 className="text-center mt-5">Героев пока нет</h5>
-        }
+	  const onDelete = useCallback((id) => {
+		deleteHero(id)
+		// eslint-disable-next-line
+	}, [])
 
-        return arr.map(({id, ...props}) => {
-            return <HeroesListItem 
-                    {...props}
-                    className='hero'
-                    key={id} 
-                    onDelete={() => onDelete(id)}
-                    />
-        })
-    }
+	if (isLoading) {
+		return <Spinner />;
+	} else if (isError) {
+		return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
+	}
 
-    const elements = renderHeroesList(filteredHeroes);
-    return (
-        <ul>
-            {elements}
-        </ul>
-    )
-}
+	const renderHeroesList = (data) => {
+		if (data.length === 0) {
+		return <h5 className="text-center mt-5">Героев пока нет</h5>;
+		}
+
+		return data.map(({ id, ...props }) => {
+		return (
+			<HeroesListItem
+			{...props}
+			className="hero"
+			key={id}
+			onDelete={() => onDelete(id)}
+			/>
+		);
+		});
+	};
+
+	return <ul>{renderHeroesList(filteredHeroes)}</ul>;
+	};
 
 export default HeroesList;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./components/app/App";
 import store from "./store";
 
 import "./styles/index.scss";
 
+const queryClient = new QueryClient();
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-	<Provider store={store}>
-		<App />
-	</Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </QueryClientProvider>
 );
